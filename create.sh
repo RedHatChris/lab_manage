280298fa9050b3e015d4bc848871204089cd3c57
Chris Hammer <chris.hammer@redhat.com>
2021-02-17 19:15:02 -0500

    0.1.6-DEV-20210217-191449 --------------------------     * MODIFY:       - create.sh - updated script to copy generated inventory file and use vault         password file instead of having to type it in each time       - manage_infra.yml - changed hosts from all to vm_host to get rid of fluff       - plays/ssh_setup.yml - now using with_fileglob to pull in the ssh keys         instead of using an array variable; less bloat       - vars/lab_env.yml - vars added for use with with_fileglob       - vars/lab_env.yml - template reference updated to reflect where/how we         wish to run the lab now; new storage host       - vars/lab_env.yml - commented out extra nodes; using a minimal env for now
        * REMOVE:
          - plays/create_env.yml - removed when exception as it's no longer needed

--- create.sh
+++ create.sh
@@ -2,6 +2,7 @@
 ansible-playbook -i inventory/lab_infra.yml manage_infra.yml --tags create
 sleep 30
 ansible-playbook -i inventory/lab_infra.yml manage_infra.yml --tags inventory
-ansible-playbook manage_nodes.yml --ask-vault-pass --tags register
-ansible-playbook manage_nodes.yml --ask-vault-pass
+cp -av /home/chris/git/RedHat/ansible/inventory/lab_inventory.yml /home/chris/git/RedHat/ansible/plays/ssc.tower/inventory/ssc_inventory.yml
+ansible-playbook manage_nodes.yml --vault-password-file vault/.vp.io --tags register
+ansible-playbook manage_nodes.yml --vault-password-file vault/.vp.io
 ansible-playbook -i inventory/lab_infra.yml manage_infra.yml --tags snap
