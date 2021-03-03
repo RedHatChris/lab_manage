820a14a00d39b152035cc93879d0a2385473c3ee
Chris Hammer <chris.hammer@redhat.com>
2021-03-03 10:49:33 -0500

    0.1.7-DEV-20210303-104801 -------------------------     * ADD:       - destroy.sh - add vault pwfile and facts removal
        MODIFY:
          - vars/lab_env.yml - update templates and modify nodes to have 2 cores instead of 1

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ansible-playbook manage_nodes.yml --ask-vault-pass --tags unregister
+ansible-playbook manage_nodes.yml --vault-password-file vault/.vp.io --tags unregister
 ansible-playbook -i inventory/lab_infra.yml manage_infra.yml --tags delsnap
 ansible-playbook -i inventory/lab_infra.yml manage_infra.yml --tags destroy
+rm -fv /etc/ansible/facts.d/*
