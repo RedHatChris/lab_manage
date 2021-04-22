d855b1aa84a0ed36d9028746f0cea208f388f03b
Chris Hammer <chris.hammer@redhat.com>
2021-04-22 15:02:11 -0400

    0.1.9-DEV-20210422-150139 -------------------------     * MODIFY:       - destroy.sh - remove only lab related fact files

--- destroy.sh
+++ destroy.sh
@@ -2,4 +2,4 @@
 ansible-playbook manage_nodes.yml --vault-password-file vault/.vp.io --tags unregister
 ansible-playbook -i inventory/lab_infra.yml manage_infra.yml --tags delsnap
 ansible-playbook -i inventory/lab_infra.yml manage_infra.yml --tags destroy
-rm -fv /etc/ansible/facts.d/*
+rm -fv /etc/ansible/facts.d/lab-*
